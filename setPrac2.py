f59cbd0bfad45e97f072eda1fc4dc57f0a348b86
Soumya Manna <drasec.04@gmail.com>
2022-12-04 12:18:28 +0530

    Learning

--- setPrac2.py
+++ setPrac2.py
@@ -1,4 +1,8 @@
-mySet = {"car", "boat", "train"}
-
-mySet.add("jet")
-print(mySet)
+mySet1 = {"car", "boat", "train"}
+mySet2 = {1,2,3}
+mySet1.add("jet")
+print(mySet1)
+mySet1.update(mySet2)
+print(mySet1)
+mySet1.discard("boat")
+print(mySet1)
